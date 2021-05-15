1af2d4489c8fdf95f995b259173ad9eca16808c0
justcris <cristian.scapin654@gmail.com>
2021-05-15 14:41:39 +0200

    store created, now i have to implement the search

--- php/app/store/app.js
+++ php/app/store/app.js
@@ -19,29 +19,113 @@
 //     }
 // }
 
-//ajax usando jquery
-function search_articles() {
-    $.ajax(
-        'search.php',
-        {
-            success: function (data) {
-                let json_parsed = JSON.parse(data);
-                console.log(json_parsed);
-                console.log(data)
-                fill_div(data)
-            },
-            error: function () {
-                console.log('error');
-            }
-        }
-    );
-}
 
 function fill_div(data) {
-    let articles_div = document.getElementById('articles-container');
-    articles_div.innerHTML = '';
-    let articles = JSON.parse(data);
-    for (let i = 0; i < articles.length; i++) {
-        articles_div.innerHTML += articles[i].articleCard;
-    }
+    let articles_div = document.getElementById('articles');
+    // articles_div.innerHTML = '';
+    json_parsed = JSON.parse(data);
+    articles = json_parsed;
+    console.log(articles)
+
+
+    // articles_div.innerHTML = `<card-article
+    //                             v-for="item in articles"
+    //                             :prodname="item.productName"
+    //                             :price="item.price"
+    //                             :key="item.id"
+    //                             >
+    //                             </card-article>`;
 }
+
+let articles = [
+    {product_name: 'RTX 3070', price: 400, brand: 'ciao', category: 'ciao2', articleCard: ''},
+    {product_name: 'RTX 3070', price: 401, brand: 'ciao', category: 'ciao2', articleCard: ''},
+    {product_name: 'RTX 3070', price: 402, brand: 'ciao', category: 'ciao2', articleCard: ''},
+];
+
+// function fill_div(data) {
+//     let articles_div = document.getElementById('articles');
+//     articles_div.innerHTML = '';
+//     let articles = JSON.parse(data);
+//     for (let i = 0; i < articles.length; i++) {
+//         articles_div.innerHTML += articles[i].articleCard;
+//     }
+// }
+
+
+Vue.component('articles-container', {
+    data() {
+        return {
+            articles: {},
+        };
+    },
+    methods: {
+        //ajax usando jquery
+        search_articles: () => {
+            $.ajax(
+                'search.php',
+                {
+                    success: (data) => {
+                        // let json_parsed = JSON.parse(data);
+                        // console.log(json_parsed);
+                        console.log(this.articles)
+                        this.articles = JSON.parse(data);
+                        //console.log(this.articles)
+
+                    },
+                    error: function () {
+                        console.log('error');
+                    }
+                }
+            );
+        }
+    },
+    mounted: () => {
+        this.articles = articles;
+    },
+    computed: {
+        get_articles: () =>{
+            return this.articles
+        }
+    }
+})
+
+
+const app = Vue.createApp(ArticleCard);
+
+app.component('card-article', {
+    props: {
+        prodname: String,
+        price: Number,
+    },
+    template: `
+      <div class="col">
+      <div class="card shadow-sm">
+        <svg class="bd-placeholder-img card-img-top" width="100%" height="225"
+             xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
+             preserveAspectRatio="xMidYMid slice" focusable="false"><title>{{ prodname }}</title>
+          <image href="https://images-na.ssl-images-amazon.com/images/I/51F79GDGXGL._AC_SL1000_.jpg"
+                 height="100%" width="100%"></image>
+        </svg>
+
+        <div class="card-body">
+          <p class="card-text">{{ prodname }}</p>
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group">
+              <button type="button" class="btn btn-sm btn-outline-secondary">Dettagli</button>
+
+            </div>
+            <button type="button" class="price btn btn-warning">
+              {{ price }} €
+            </button>
+          </div>
+        </div>
+      </div>
+      </div>`,
+    mounted: () => {
+        console.log('ciao')
+    }
+});
+
+app.mount('#app');
+
--- php/app/store/store.php
+++ php/app/store/store.php
@@ -89,33 +89,48 @@ if (isset($_SESSION['name']) and isset($_SESSION['surname'])) {
     </div>
 </header>
 
-<!-- eseguire una ricerca con ajax -->
-<div class="bg-light form-control">
-    <label for="search">Cerca:</label>
-    <input type="text" class="form-control" name="search" id="search" onkeyup="search_articles(this.value)">
-</div>
+<div id="app">
+    <!-- eseguire una ricerca con ajax -->
+    <div class="bg-light form-control">
+        <label for="search">Cerca:</label>
+        <input type="text" class="form-control" name="search" id="search" @keyup="search_articles(this.value)">
+    </div>
 
-<div class="album py-5 bg-light">
-    <div class="container">
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3" id="articles-container">
+    <div class="album py-5 bg-light">
+        <div class="container">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
 
-            <!--            cards              -->
-            <?php
-            $cards = array();
-            for ($i = 0; $i < 2; $i++) {
-                array_push($cards,
-                    new Product("RTX 207$i", 'manto', 'Schede Video', 'NVIDIA', 400.0 + $i));
-            }
+                <!--            cards              -->
+                <!--            --><?php
+                //            $cards = array();
+                //            for ($i = 0; $i < 2; $i++) {
+                //                array_push($cards,
+                //                    new Product("RTX 207$i", 'manto', 'Schede Video', 'NVIDIA', 400.0 + $i));
+                //            }
+                //
+                //            foreach ($cards as $card) {
+                //                echo $card->getCard();
+                //            }
+                //
+                //            ?>
 
-            foreach ($cards as $card) {
-                echo $card->getCard();
-            }
 
-            ?>
+                <card-article
+                        v-for="item in get_articles"
+                        :prodname="item.product_name"
+                        :price="item.price"
+                        :key="item.product_name"
+                >
+                </card-article>
+
+            </div>
         </div>
     </div>
 </div>
 
+
+<!--<script src="https://unpkg.com/vue@next"></script>-->
+<script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
 <script src="app.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8"
