2ce35f88dca03e90205a9dd63edc6883259f25cc
justcris <cristian.scapin654@gmail.com>
2021-05-25 18:41:07 +0200

    added about_us.php and visit_us.php pages and fixed the header on top of every page

--- php/user/login.php
+++ php/user/login.php
@@ -6,59 +6,38 @@ $conn = connect_db("root", "", "db_catena_negozi_2");
 
 if (isset($_REQUEST['submit'])) {
 //    echo "1";
+    $stmt = null;
     if ($_POST['user_type'] == 'user') {
         //select con prepared statement
         $stmt = $conn->prepare("SELECT nome, cognome, password FROM db_catena_negozi_2.Utente where email=?");
-        $stmt->bind_param("s", $email);    //s = string
-
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-
-        $stmt->execute();
-        $result = $stmt->get_result();
-//    var_dump($result);
-        if ($result->num_rows == 1) {
-            $row = $result->fetch_assoc();
-            $conn->close();
-            $psw_hashed = $row['password'];
-//        echo "$psw_hashed";
-            if (password_verify($password, $psw_hashed)) {
-                $_SESSION['user_type'] = $_POST['user_type'];
-                $_SESSION['name'] = $row['nome'];
-                $_SESSION['surname'] = $row['cognome'];
-                $_SESSION['email'] = $email;
-                header("Location: ../app/homepage.php");
-                exit();
-            }
-        }
-        $conn->close();
     } elseif ($_POST['user_type'] == 'employee' and $_POST['badge'] != '') {
 
         //select con prepared statement
         $stmt = $conn->prepare("SELECT nome, cognome, password FROM db_catena_negozi_2.Dipendente where email=?");
-        $stmt->bind_param("s", $email);    //s = string
-
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-
-        $stmt->execute();
-        $result = $stmt->get_result();
-        var_dump($result);
-        if ($result->num_rows == 1) {
-            $row = $result->fetch_assoc();
-            $conn->close();
-            $psw_hashed = $row['password'];
-            if (password_verify($password, $psw_hashed)) {
-                $_SESSION['user_type'] = $_POST['user_type'];
-                $_SESSION['name'] = $row['nome'];
-                $_SESSION['surname'] = $row['cognome'];
-                $_SESSION['email'] = $email;
-                header("Location: ../app/homepage.php");
-                exit();
-            }
-        }
-        $conn->close();
     }
+    $stmt->bind_param("s", $email);    //s = string
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $conn->close();
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        $psw_hashed = $row['password'];
+        if (password_verify($password, $psw_hashed)) {
+            $_SESSION['user_type'] = $_POST['user_type'];
+            $_SESSION['name'] = $row['nome'];
+            $_SESSION['surname'] = $row['cognome'];
+            $_SESSION['email'] = $email;
+            header("Location: ../app/homepage.php");
+            exit();
+        }
+    }
+    header("Location: login.html?msg=error");
+    exit();
+
 }
 $conn->close();
 header("Location: login.html?msg=error");
